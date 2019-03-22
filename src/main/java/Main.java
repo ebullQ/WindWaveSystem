aafe02947d9ab885249a441f1f66bb5f3d690b2f
Matvei <bull3003@gmail.com>
2019-03-22 19:09:57 +0300

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import service.ImageCreatorService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new ImageCreatorService().makeImage();
+
+        System.out.println();
+    }
+
+
+
+
+
+}
